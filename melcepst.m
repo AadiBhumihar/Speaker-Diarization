63b2822119273b1e2413dd695940751578984e11
Aditya Raj <201451032@iiitvadodara.ac.in>
2017-05-30 14:19:49 +0530

    Add files via upload

---
+++ melcepst.m
@@ -0,0 +1,51 @@
+function c=melcepst(s,fs,w,nc,p,n,inc,fl,fh)
+%MELCEPST Calculate the mel cepstrum of a signal C=(S,FS,W,NC,P,N,INC,FL,FH)
+
+if nargin<2 fs=8000; end
+if nargin<3 w='M'; end
+if nargin<4 nc=12; end
+if nargin<5 p=floor(3*log(fs)); end
+if nargin<6 n=pow2(floor(log2(0.03*fs))); end
+if nargin<9
+  fh=0.5;   
+  if nargin<8
+    fl=0;
+    if nargin<7
+      inc=floor(n/2);
+    end
+  end
+end
+
+%w='M';
+%nc=12;
+%p=floor(3*log(fs)); 
+%n=pow2(floor(log2(0.03*fs)));
+
+ %  fh=0.5;   
+ % fl=0;
+ % inc=floor(n/2);
+    
+  
+if length(w)==0
+   w='M';
+end
+
+   z=enframe(s,hamming(n),inc);
+
+f=rfft(z.');
+[m,a,b]=melbankm(p,n,fs,fl,fh,w);
+
+pw=f(a:b,:).*conj(f(a:b,:));
+pth=max(pw(:))*1E-6;
+   
+ath=sqrt(pth);
+   y=log(max(m*abs(f(a:b,:)),ath));
+
+c=rdct(y).';
+nf=size(c,1);
+nc=nc+1;
+if p>nc
+   c(:,nc+1:end)=[];
+elseif p<nc
+   c=[c zeros(nf,nc-p)];
+end
